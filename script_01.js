f07a2a7c0b3dfce23f0a280b90fae13f15f7f3fa
Marinus <marinus.fehling@web.de>
2023-01-17 14:27:14 +0100

    Trennen der Funktionalitäten, return

--- script_01.js
+++ script_01.js
@@ -5,11 +5,11 @@
 // 1. Kapselung von Codeblöcken
 
 // Funktionsaufruf (call)
-test();
-ausgabeName();
-ausgabeNameParam("Minus");
-ausgabeNameParams("Marinus", "Plus");
-ausgabeNameParams(prompt("Vorname eingeben:"), prompt("Nachname eingeben:"));
+// test();
+// ausgabeName();
+// ausgabeNameParam("Minus");
+// ausgabeNameParams("Marinus", "Plus");
+// ausgabeNameParams(prompt("Vorname eingeben:"), prompt("Nachname eingeben:"));
 
 // Funktionsrumpf (body) | callee
 // Funktionsdeklaration
@@ -38,4 +38,44 @@ function ausgabeNameParam(user)
 function ausgabeNameParams(firstname, familyname)
 {
     console.log ("Hallo " + firstname + " " + familyname);
+}
+
+/***** Funktionen 03a *****/
+
+// 03a. Vorbereitung
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+
+// Funktionsaufruf
+//ausgabeNameParams2("Minus", "Plus");
+
+function ausgabeNameParams2(firstname, familyname)
+{
+    // 1. Funktionalität: string composing
+    const GAP = " ";
+    let str = "Hallo" + GAP + firstname + GAP + familyname;
+
+    // 2. Funktionalität: string output
+    console.log(str);
+    
+}
+
+// Trennen der Funktionalitäten | return
+
+// Funktionsaufruf
+output(getString("Minus", "Plus"));
+
+// 1. Funktionalität: string composing
+function getString(firstname, familyname)
+{
+    const GAP = " ";
+    let str = "Hallo" + GAP + firstname + GAP + familyname;
+    return str;
+}
+
+// 2. Funktionalität: string output
+function output(outputData)
+{
+    console.log(outputData);
 }
