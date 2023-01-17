684059a3db35e1d11242659d44ed7c8b6c2eaf3a
Marinus <marinus.fehling@web.de>
2023-01-17 11:15:28 +0100

    Funktionen, Datenübergabe von Innen

--- script_01.js
+++ script_01.js
@@ -4,3 +4,21 @@
 
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+test();
+ausgabeName()
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+function test()
+{
+console.log("Habedere");
+}
+
+// 2a Parametrisierung + Datenübergabe von INNEN
+
+function ausgabeName()
+{
+    let user = "Marinus";
+    console.log ("Hallo " + user);
+}
